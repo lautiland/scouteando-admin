5f4a307e2c8557468c666d1aeb159955c775d9f1
lautiland <ljovanovics@fi.uba.ar>
2025-07-10 12:58:58 -0300

    Fix: Agregar driver PostgreSQL (pg) para Render deployment
    - Agregado pg@^8.11.0 a las dependencias
    - Actualizada configuración para Render (PORT=10000)
    - Eliminada advertencia deprecada STRAPI_DISABLE_UPDATE_NOTIFICATION
    - Configurado logger.updates.enabled en server.js

--- config/server.js
+++ config/server.js
@@ -7,4 +7,9 @@ module.exports = ({ env }) => ({
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
   },
+  logger: {
+    updates: {
+      enabled: false
+    }
+  },
 });
